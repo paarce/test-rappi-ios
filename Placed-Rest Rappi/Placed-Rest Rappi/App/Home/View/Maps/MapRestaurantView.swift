34d41f29dc2c55503f6080474192fddf8b46d05e
Augusto Cordero <augusto100295@gmail.com>
2019-10-13 22:05:15 -0400

    set section maps

---
+++ Placed-Rest Rappi/Placed-Rest Rappi/App/Home/View/Maps/MapRestaurantView.swift
@@ -0,0 +1,92 @@
+//
+//  MapRestaurantView.swift
+//  Placed-Rest Rappi
+//
+//  Created by Augusto C.P. on 10/13/19.
+//  Copyright © 2019 Augusto C.P. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import SnapKit
+
+class MapRestaurantView: UIView {
+
+    var mapView: MKMapView?
+    
+    func iniUI(location : CLLocation, regionRadius : CLLocationDistance ) {
+        self.backgroundColor = UIColor.green
+        self.configMapView()
+        self.centerMapOnLocation(location: location, regionRadius: regionRadius)
+    }
+    
+    func configMapView() {
+        
+        mapView = MKMapView(frame: self.frame)
+        mapView!.delegate = self
+        mapView!.register(RestaurantPinView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+        self.addSubview(self.mapView!)
+        mapView?.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalToSuperview()
+        }
+        
+    }
+    
+    // MARK: - CLLocationManager
+    
+    let locationManager = CLLocationManager()
+    
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            mapView?.showsUserLocation = true
+            
+        } else {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+    func centerMapOnLocation(location: CLLocation, regionRadius : CLLocationDistance) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView?.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
+
+}
+
+
+// MARK: - MKMapViewDelegate
+
+extension MapRestaurantView: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView,
+                 calloutAccessoryControlTapped control: UIControl) {
+        
+        let pin = view.annotation as! RestaurantPin
+        
+        let vc = R.storyboard.home.restaurantDetailViewController()
+        vc!.data = pin.data
+        //self.navigationController?.pushViewController(vc!, animated: true)
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation { return nil }
+        
+        if let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "") {
+            annotationView.annotation = annotation
+            return annotationView
+        } else {
+            let annotationView = MKPinAnnotationView(annotation:annotation, reuseIdentifier:"")
+            annotationView.isEnabled = true
+            annotationView.canShowCallout = true
+            
+            let btn = UIButton(type: .infoDark)
+            annotationView.leftCalloutAccessoryView = btn
+            return annotationView
+        }
+    }
+    
+}
--- Placed-Rest Rappi/Placed-Rest Rappi/App/Home/ViewController/HomeViewController.swift
+++ Placed-Rest Rappi/Placed-Rest Rappi/App/Home/ViewController/HomeViewController.swift
@@ -12,7 +12,6 @@ import MapKit
 class HomeViewController: UIViewController {
 
     // MARK: - Properties
-    @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var menuTabBarView: NavigationTabBar!{
         didSet{
             let color = UIColor.init(red: 146/255, green: 126/255, blue: 71/255, alpha: 1)
@@ -23,6 +22,8 @@ class HomeViewController: UIViewController {
             menuTabBarView.selectorTextColor   = colorText
         }
     }
+    
+    @IBOutlet weak var mapRestaurantView: MapRestaurantView!
     
     var homeVM = HomeViewModel()
     
@@ -41,39 +42,20 @@ class HomeViewController: UIViewController {
         
         
         self.configSearchBar()
+        self.mapRestaurantView.iniUI(location: self.homeVM.searchLocation, regionRadius: self.homeVM.regionRadius)
         
-        centerMapOnLocation(location: self.homeVM.searchLocation)
         
-        mapView.delegate = self
-        mapView.register(RestaurantPinView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
         
         self.loadInitialData()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        checkLocationAuthorizationStatus()
+        self.mapRestaurantView.checkLocationAuthorizationStatus()
     }
     
-    // MARK: - CLLocationManager
-    
-    let locationManager = CLLocationManager()
-    func checkLocationAuthorizationStatus() {
-        if CLLocationManager.authorizationStatus() == .authorizedAlways {
-            mapView.showsUserLocation = true
-            
-        } else {
-            locationManager.requestAlwaysAuthorization()
-        }
-    }
     
     // MARK: - Helper methods
-    
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
-                                                  latitudinalMeters: homeVM.regionRadius, longitudinalMeters: homeVM.regionRadius)
-        mapView.setRegion(coordinateRegion, animated: true)
-    }
     
     func loadInitialData() {
         homeVM.callSearchObservable{ result in
@@ -81,7 +63,7 @@ class HomeViewController: UIViewController {
                 case .success(let model):
                     if let restaurants = model.restaurants {
                         let pins = restaurants.compactMap { RestaurantPin(data: $0) }
-                        self.mapView.addAnnotations(pins)
+                        self.mapRestaurantView.mapView?.addAnnotations(pins)
                     }
                 break
 
@@ -93,41 +75,6 @@ class HomeViewController: UIViewController {
     }
     
 }
-
-// MARK: - MKMapViewDelegate
-
-extension HomeViewController: MKMapViewDelegate {
-   
-    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView,
-                 calloutAccessoryControlTapped control: UIControl) {
-        
-        let pin = view.annotation as! RestaurantPin
-        
-        let vc = R.storyboard.home.restaurantDetailViewController()
-        vc!.data = pin.data
-        self.navigationController?.pushViewController(vc!, animated: true)
-        
-    }
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if annotation is MKUserLocation { return nil }
-        
-        if let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "") {
-            annotationView.annotation = annotation
-            return annotationView
-        } else {
-            let annotationView = MKPinAnnotationView(annotation:annotation, reuseIdentifier:"")
-            annotationView.isEnabled = true
-            annotationView.canShowCallout = true
-            
-            let btn = UIButton(type: .infoDark)
-            annotationView.leftCalloutAccessoryView = btn
-            return annotationView
-        }
-    }
-    
-}
-
 
 extension HomeViewController: UISearchResultsUpdating, UISearchBarDelegate {
     
